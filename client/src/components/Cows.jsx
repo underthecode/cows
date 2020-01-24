0ac766154c8ba0e0374b9fc086935dd79705bcd2
Steven Santos <stevensantos@Stevens-MacBook-Pro-2.local>
2020-01-24 15:54:45 -0800

    init first Cows component

---
+++ client/src/components/Cows.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import axios from 'axios';
+
+class Cows extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Cows</h1>
+      </div>
+    );
+  }
+}
+
+export default Cows;
