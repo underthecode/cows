cb362104f9915e02f8d05028fab0e351dccb7354
スティーブンさん <underyourcover@icloud.com>
2020-01-26 01:28:20 -0800

    imported css

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import css from './styles/Buttons.css';
 import Cows from './components/Cows.jsx';
 
 ReactDOM.render(<Cows />, document.getElementById('cows-app'));
