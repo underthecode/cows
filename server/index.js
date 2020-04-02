839dd741eba1b1bb049d563c741ad8b7e7b3308e
スティーブンさん <underyourcover@icloud.com>
2020-04-01 22:46:41 -0700

    removed body-parser, express.json instead

--- server/index.js
+++ server/index.js
@@ -1,16 +1,10 @@
 const express = require('express');
 const path = require('path');
-const bodyParser = require('body-parser');
 const routes = require('./routes/routes');
 
 const app = express();
 
-app.use(bodyParser.json());
-app.use(
-  bodyParser.urlencoded({
-    extended: true
-  })
-);
+app.use(express.json());
 
 app.use(express.static(path.join(__dirname, '../client/dist')));
 
