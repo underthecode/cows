d241584e44d3140c42fa1aaa2705d18555de2621
スティーブンさん <underyourcover@icloud.com>
2020-04-04 01:53:35 -0700

    added GET one id and PATCH id

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -2,10 +2,9 @@ const express = require('express');
 const router = express.Router();
 const models = require('../models/Cow');
 
-router.post('/api/cows', (req, res) => {
-  const { id, name, description } = req.body;
+router.post('/api/cow', (req, res) => {
+  const { name, description } = req.body;
   const data = new models.Cow({
-    id,
     name,
     description
   });
@@ -21,13 +20,49 @@ router.post('/api/cows', (req, res) => {
 });
 
 router.get('/api/cows', (req, res) => {
-  models.Cow.find((err, data) => {
-    if (err) {
-      console.log(err);
-    } else {
+  models.Cow.find()
+    .then(data => {
       res.status(200).json(data);
-    }
-  });
+    })
+    .catch(err => {
+      res.status(400).send(err);
+    });
+});
+
+router.get('/api/cow/:id', (req, res) => {
+  const { id } = req.params;
+
+  models.Cow.findById(id)
+    .then(data => {
+      res.status(200).json(data);
+    })
+    .catch(err => {
+      res.status(400).send(err);
+    });
+});
+
+router.patch('/api/cow/:id', (req, res) => {
+  const { id } = req.params;
+
+  models.Cow.findById(id)
+    .then(data => {
+      const { name, description } = req.body;
+
+      if (name) {
+        data.name = name;
+      }
+
+      if (description) {
+        data.description = description;
+      }
+
+      data.save().then(data => {
+        res.status(200).json(data);
+      });
+    })
+    .catch(err => {
+      res.status(400).send(err);
+    });
 });
 
 module.exports = router;
