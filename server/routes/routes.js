b5e7d16e07ed5871171fdbc98c151e181f4626ef
スティーブンさん <underyourcover@icloud.com>
2020-04-04 00:08:04 -0700

    updated to Cow

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const models = require('../models/Cows');
+const models = require('../models/Cow');
 
 router.post('/api/cows', (req, res) => {
   const { id, name, description } = req.body;
