a18f0d8fbfa7c401f56bcdd67b2a4c5a5463f1a7
スティーブンさん <underyourcover@icloud.com>
2020-04-06 01:54:21 -0700

    configured react-hot-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,47 @@
 const path = require('path');
+const webpack = require('webpack');
 const SRC_DIR = path.join(__dirname, '/frontend/src');
 const DIST_DIR = path.join(__dirname, '/frontend/dist');
 
 module.exports = {
   entry: `${SRC_DIR}/index.jsx`,
-  output: {
-    filename: 'bundle.js',
-    path: DIST_DIR
-  },
+  mode: 'development',
   module: {
     rules: [
       {
-        test: /\.jsx?/,
+        test: /\.(js|jsx)$/,
         exclude: /(node_modules|bower_components)/,
         include: SRC_DIR,
         loader: 'babel-loader',
         options: {
-          presets: ['@babel/preset-env', '@babel/preset-react']
-        }
+          presets: ['@babel/preset-env', '@babel/preset-react'],
+        },
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+          },
+        ],
       },
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader']
-      }
-    ]
-  }
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  resolve: { extensions: ['*', '.js', '.jsx'] },
+  output: {
+    filename: 'bundle.js',
+    path: DIST_DIR,
+    publicPath: '/dist/',
+  },
+  devServer: {
+    contentBase: DIST_DIR,
+    port: 3000,
+    publicPath: 'http://localhost:3000/dist/',
+    hotOnly: true,
+  },
+  plugins: [new webpack.HotModuleReplacementPlugin()],
 };
