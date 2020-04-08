6b1ca796fb5a2422c0a50f6fbc8ebac72f04b63a
スティーブンさん <underyourcover@icloud.com>
2020-04-08 16:48:20 -0700

    boilerplate home page with minimal Bootstrap

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,11 +1,48 @@
+import { hot } from 'react-hot-loader';
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  NavLink,
+  Switch
+} from 'react-router-dom';
+import Home from './root/Home.jsx';
 
 const App = () => {
   return (
     <div className='app'>
-      <h1>Hello</h1>
+      <Router>
+        <Navigation />
+        <div className='container'>
+          <Main />
+        </div>
+      </Router>
     </div>
   );
 };
 
-export default App;
+const Navigation = () => {
+  return (
+    <nav className='navbar navbar-expand-lg navbar-dark bg-dark mb-4'>
+      <div className='container'>
+        <ul className='navbar-nav mr-auto'>
+          <li className='nav-item'>
+            <NavLink exact className='nav-link' activeClassName='active' to='/'>
+              Home
+            </NavLink>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+const Main = () => {
+  return (
+    <Switch>
+      <Route exact path='/' component={Home} />
+    </Switch>
+  );
+};
+
+export default hot(module)(App);
---
+++ frontend/src/components/root/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div className='jumbotron'>
+      <h1 className='display-4'>Home</h1>
+      <p className='lead'>A place to organize cows</p>
+      <hr className='my-4'></hr>
+    </div>
+  );
+};
+
+export default Home;
